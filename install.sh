6d52be187bee149866f0c86d52ca1e98597c3bb1
David Neaz <david.neaz@gmail.com>
2018-05-04 09:03:14 -0700

    Install script to install command line requirements

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Install requirements
+
+sudo apt-get update
+
+# install node
+sudo apt-get install -y nodejs
+
+# install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get install yarn
+
+# install npm libraries
+yarn add babel-cli babel-core babel-eslint babel-loader
+yarn add prettier
+yarn add eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
+yarn add axios
