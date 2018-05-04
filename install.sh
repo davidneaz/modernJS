2b6095aa80262adc52c1a3eee3a3a5a68635414e
David Neaz <david.neaz@gmail.com>
2018-05-04 09:08:33 -0700

    Add yarn init and don't prompt for installation

--- install.sh
+++ install.sh
@@ -10,9 +10,10 @@ sudo apt install -y nodejs
 # install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt install yarn
+sudo apt install -y yarn
 
 # install npm libraries
+yarn init
 yarn add babel-cli babel-core babel-eslint babel-loader
 yarn add prettier
 yarn add eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
