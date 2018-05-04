e7bc8705e02db8ba21fdbbd768038708297de780
David Neaz <david.neaz@gmail.com>
2018-05-04 10:38:39 -0700

    Install babel presets.

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ sudo apt install -y yarn
 # install npm libraries
 yarn init
 yarn add babel-cli babel-core babel-eslint babel-loader
+yarn add babel-preset-env babel-preset-es2015 babel-preset-react babel-preset-stage-0
 yarn add prettier
 yarn add eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
 yarn add axios
