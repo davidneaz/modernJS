0478870151d4497919f124a331d07aa697340322
David Neaz <david.neaz@gmail.com>
2018-05-04 09:20:45 -0700

    Just yarn

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install -y yarnpkg
+sudo apt install -y yarn
 
 # install npm libraries
 yarn init
